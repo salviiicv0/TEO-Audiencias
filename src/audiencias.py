7e317ee7edee06eb5dc5d09d2fef3892b12c1b0a
salviiicv0 <salcarvaz@alum.us.es>
2024-10-01 16:47:15 +0200

    apartado2

--- src/audiencias.py
+++ src/audiencias.py
@@ -1,7 +1,7 @@
 import csv # Para leer los csv de datos
 from matplotlib import pyplot as plt # Para mostrar gráficas, importamos matplotlib.pyplot, renombrándolo como plt
 from collections import namedtuple # Para definir tuplas con nombre
-Audiencia = namedtuple ("Audiencia", "edición, share")
+Audiencia = namedtuple ("Audiencia", "edicion, share")
 
 def lee_Audiencia(fichero):
     '''
@@ -31,3 +31,90 @@ def lee_Audiencia(fichero):
             share = float(share)
             audiencias.append(Audiencia(edicion, share))
         return audiencias
+    
+def calcula_ediciones(audiencias):
+    '''
+    Calcula el número de ediciones presentes en una lista de audiencias
+
+    ENTRADA:
+        @param audiencias: lista de tuplas de audiencias
+        @type audiencias: [Audiencias(int, float)]
+    SALIDA:
+        @return: El número de ediciones del programa
+        @rtype: int
+
+    Toma como entrada una lista de tuplas (edición, share) y produce como
+    salida el número de ediciones del programa
+    Solución utilizando una definición de audiencias por compresión
+    '''
+
+    #calculamos el conjunto de ediciones presentes
+    ediciones = set()
+    for a in audiencias:
+        ediciones.add(a.edicion)
+
+    #devolvemos el tamaño del conjunto
+    return len(ediciones)
+
+def filtra_por_ediciones(audiencias, ediciones):
+    '''
+    Selecciona las tuplas correspondientes a unas determinadas ediciones
+
+    ENTRADA:
+        @param audiencias: lista de audiencias
+        @type audiencias: [Audiencia(int, float)]
+        @param ediciones: lista de ediciones a seleccionar
+        @type ediciones:  [int]
+
+    SALIDA:
+        @return: lista de tuplas de audiencias seleccionadas
+        @rtype: [Audiencias(int, float)]
+    '''
+    filtradas = []
+    for a in audiencias:
+        if a.edicion in ediciones:
+            filtradas.append(a)
+    return filtradas
+
+def agrupa_share_por_ediciones(audiencias):
+    '''
+    Para una lista de tuplas de audiencias, agrupa los valores de share por edición en un diccionario.
+
+    ENTRADA:
+        @param audiencias: lista de tuplas (edicion, share) con las audiencias de un programa de televisión
+        @type audiencias: [Audiencias(int, float)]
+    SALIDA:
+        @return: Un diccionario en el que las claves son las ediciones y los valores son listas de shares de cada edición
+        @rtype: {int:[float]}
+    '''
+    res = dict() #Crea diccionario vacío, también se puede hacer res = {}
+    for a in audiencias:
+        if a.edicion in res:
+            #Si la edición ya está en el diccionario
+            res[a.edicion].append(a.share) #Se añade el share a la lista de shares asociada a esa edición
+        else:
+            #Si la edición no está en el diccionario
+            res[a.edicion] = [a.share] # Se crea una nueva lista con el share y se asocia a la edición
+    return res
+
+def medias_por_ediciones(audiencias):
+    '''
+    Calcula la media de audiencia para cada edición
+
+    ENTRADA:
+        @param audiencias: lista de audiencias
+        @type audiencias: [Audiencia(int, float)]
+    SALIDA:
+        @return: medias de audiencia por cada edición
+        @rtype: {int: float}
+
+    Toma como entrada una lista de tuplas (edición, share) y produce como
+    salida un diccionario en el que las claves son las ediciones y los
+    valores son las medias de audiencia de cada edición.
+    '''
+
+    share_por_ediciones = agrupa_share_por_ediciones(audiencias)
+    medias = dict()
+    for edicion, lista_shares in share_por_ediciones.items():
+        medias[edicion] = sum(lista_shares) / len(lista_shares)
+    return medias
--- src/audiencias_test.py
+++ src/audiencias_test.py
@@ -4,6 +4,22 @@ def test_lee_audiencias(audiencias, nombre_programa):
     print(f"Audiencias del programa {nombre_programa}")
     print(audiencias[:20])
 
+def test_calcula_ediciones(audiencias, nombre_programa):
+    ediciones = calcula_ediciones(audiencias)
+    print(f"Número de ediciones del program {nombre_programa}: {ediciones}")
+
+def test_filtra_por_ediciones(audiencias, ediciones, nombre_programa):
+    filtradas = filtra_por_ediciones(audiencias, ediciones)
+    print(f"Los datos de las ediciones {ediciones} del programa {nombre_programa} son: \n {filtradas}")
+
+def test_agrupa_por_ediciones(audiencias, nombre_programa):
+    dicc = agrupa_share_por_ediciones(audiencias)
+    print(f"Agrupación por ediciones del programa {nombre_programa}: \n{dicc}")
+
+def test_medias_por_ediciones(audiencias, nombre_programa):
+    dicc = medias_por_ediciones(audiencias)
+    print(f"La media por ediciones del programa {nombre_programa} es: \n{dicc}")
+
 def main():
     '''
     Función principal
@@ -14,6 +30,20 @@ def main():
 
     audiencias_masterchef = lee_Audiencia('./data/MasterChef.csv')
     test_lee_audiencias(audiencias_masterchef, "Master Chef")
+    #test de la función calcula ediciones
+    test_calcula_ediciones(audiencias_gh, "Gran Hermano")
+    test_calcula_ediciones(audiencias_masterchef, "Master Chef")
+    #test de la función filtra_por_ediciones
+    test_filtra_por_ediciones(audiencias_gh, [1,2,3], "Gran Hermano")
+    test_filtra_por_ediciones(audiencias_masterchef, [4,5], "Master Chef")
+    #test de la función agrupa_por_ediciones
+    test_agrupa_por_ediciones(audiencias_gh, "Gran Hermano")
+    test_agrupa_por_ediciones(audiencias_masterchef, "Master Chef")
+    #test de la función media_por_ediciones
+    test_medias_por_ediciones(audiencias_gh, "Gran Hermano")
+    test_medias_por_ediciones(audiencias_masterchef, "Master Chef")
+
+
 
 if __name__ == "__main__":
     main()
